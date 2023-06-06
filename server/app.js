de1b1c94a812feec3efb3312c0ae29511e66cbc3
Kwuan <thquan20@clc.fitus.edu.vn>
2023-06-06 09:37:51 +0700

    remove routes

--- server/app.js
+++ server/app.js
@@ -10,6 +10,9 @@ const app = express();
 const errorHandler = require("./api/v1/middlewares/errorHandler.mdw");
 const responseHandler = require("./api/v1/middlewares/response.mdw");
 
+
+
+
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
