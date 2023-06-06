cf4a1a7071ea45c292a122eb732079ffbdd765f0
hau <nvhau20@clc.fitus.edu.vn>
2023-06-06 19:27:06 +0700

    fix server

--- server/app.js
+++ server/app.js
@@ -10,6 +10,7 @@ const app = express();
 const errorHandler = require("./api/v1/middlewares/errorHandler.mdw");
 const responseHandler = require("./api/v1/middlewares/response.mdw");
 
+
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
