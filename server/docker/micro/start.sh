8644f76d06bdf0397dc8ac78401c35763f673f83
DESKTOP-HKGEPO2\duong <dqvinh20@clc.fitus.edu.vn>
2023-09-10 13:00:55 +0700

    Add --remove-orphans flag

--- server/docker/micro/start.sh
+++ server/docker/micro/start.sh
@@ -3,4 +3,4 @@
 docker compose stop
 docker compose down
 docker image prune -f
-docker compose up --build -d --scale helper_services=2 --scale core_services=2
+docker compose up --build -d --scale helper_services=2 --scale core_services=2 --remove-orphans
